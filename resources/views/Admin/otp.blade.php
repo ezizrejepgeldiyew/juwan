7ce5fa353f13ec25e4796c33d60993c4660b24de
ezizrejepgeldiyew <ezizrejepgeldiyew@gmail.com>
2023-07-04 14:24:03 +0000

    sendOTP

--- resources/views/Admin/otp.blade.php
+++ resources/views/Admin/otp.blade.php
@@ -64,7 +64,9 @@
                     @if (Session::has('error'))
                         <div class="alert alert-danger alert-dismissible" role="alert">
                             <button type="button" class="close" data-dismiss="alert">
-                                <i class="fa fa-times"></i>
+                                <i class="fa fa-times">
+                                    
+                                </i>
                             </button>
                             {{ session('error') }}
                         </div>
